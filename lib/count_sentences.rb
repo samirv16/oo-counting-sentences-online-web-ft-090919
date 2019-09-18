a27ae482ce237455997c76f668d8f6cf845fd80e
edras vallecillo <samir.vallecillo@outlook.com>
2019-09-18 17:40:15 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -15,8 +15,7 @@ class String
   end
 
   def count_sentences
-    new_array = self.split(/[.!?]/).reject {|sentence| sentence.empty?}
-  end
-  new_array
+    new_array = self.split(/[.!?]/).reject {|string| string.empty?}
+  new_array.length
   end
 end
