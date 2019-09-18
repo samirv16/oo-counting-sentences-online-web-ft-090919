37bf4c17aee5171f3f4e9ccd21729753dab75631
edras vallecillo <samir.vallecillo@outlook.com>
2019-09-18 17:55:14 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,24 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    array = self.split(/[.!?]/).reject {|sentence| sentence.empty?}
+  array.length
   end
 end
+
+# array = self.split(/[.!?]/)
+# array.delete_if {|sentence| sentence.empty?}
+#   array.length
+#   end
